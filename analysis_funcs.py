fixed factor of 2

analysis_funcs.py
@@ -113,19 +113,19 @@ def upper_loss(data, alpha, beta, c, i):
     beta_opt = optimal_upper_beta(data, alpha, c, i)
     d1 = total_upper(data, alpha, beta, c, i) - upp_p
     d2 = total_upper(data, alpha, beta_opt, c, i) - upp_p
-    return 0 if np.isclose(upp_p, 0) else np.abs(d1 - d2) / upp_p
+    return 0 if np.isclose(upp_p, 0) else np.abs(d1 - d2) / d2
 
 
 def lower_loss(data, alpha, beta, c, i):
     low_p = lower_reference_point(data_to_point(data), c, i)
     beta_opt = optimal_lower_beta(data, alpha, c, i)
-    d1 = total_lower(data, alpha, beta, c, i) - low_p
-    d2 = total_lower(data, alpha, beta_opt, c, i) - low_p
-    return 0 if np.isclose(low_p, 0) else np.abs(d1 - d2) / low_p
+    d1 = low_p - total_lower(data, alpha, beta, c, i)
+    d2 = low_p - total_lower(data, alpha, beta_opt, c, i)
+    return 0 if np.isclose(low_p, 0) else np.abs(d1 - d2) / d2
 
 
 def exclude_0(max_range, num_points):
-    return np.linspace(max_range / (num_points + 1), max_range, num=num_points, endpoint=False)
+    return np.linspace(max_range / (num_points), max_range, num=num_points)
 
 
 def max_loss(max_point, alpha, beta, n0, n1, c, i, num_points=10):

beta_plots.py
@@ -37,7 +37,7 @@ def compute_beta_range(alphas, data, c, i, num_points=30):
     beta_max = 0
     for alpha in alphas:
         beta_max = max(beta_max, af.optimal_upper_beta(data, alpha, c, i))
-    beta_max = beta_max * 2
+    beta_max = beta_max /10 * num_points
     beta_range = np.linspace(beta_max / (num_points + 1), beta_max,
                              num=num_points, endpoint=False)
     return beta_range
@@ -63,10 +63,9 @@ def vary_beta_plot(alphas, data, c, i, colors=None, num_points=30,
             opt_upp_bet = af.optimal_upper_beta(data, alpha / xx, c, i)
             t_upp = af.total_upper(data, alpha / xx, opt_upp_bet, c, i)
             ax.plot(opt_upp_bet, t_upp, 'mo')
-            if xx == 2:
-                opt_low_bet = af.optimal_lower_beta(data, alpha / xx, c, i)
-                t_low = af.total_lower(data, alpha / xx, opt_low_bet, c, i)
-                ax.plot(opt_low_bet, t_low, 'mo')
+            opt_low_bet = af.optimal_lower_beta(data, alpha / xx, c, i)
+            t_low = af.total_lower(data, alpha / xx, opt_low_bet, c, i)
+            ax.plot(opt_low_bet, t_low, 'mo')
         ax.plot(beta_range, one_sided_vary_beta(alpha, data, c, i, beta_range),
                 color=color, linestyle='-',
                 label=r"one sided, level = {:.2f}".format(1 - alpha))
@@ -109,7 +108,7 @@ def main(args):
     data = af.point_to_data(args.n0, args.n1,
                             af.point_at_nominal(args.r100, args.r001,
                                                 args.r011))
-    vary_beta_plot(alphas, data, args.c, args.i, filename=args.filename)
+    vary_beta_plot(alphas, data, args.c, args.i, num_points=args.num_points, filename=args.filename)
     for alpha in alphas:
         print("alpha = {:.2e}, optimal upper beta = {:.2e}".format(alpha,
                                                                    af.optimal_upper_beta(
@@ -135,8 +134,10 @@ if __name__ == "__main__":
                         help="Nominal measurement error to compute at. Will take r100 = r010")
     parser.add_argument("--r011", type=float, default=1e-4,
                         help="Nominal measurement error to compute at. Will take r011 = r101")
-    parser.add_argument("--r001", type=float, default=1e-4,
+    parser.add_argument("--r001", type=float, default=1e-2,
                         help="Nominal prep error to compute at. Will take r001 = r110")
+    parser.add_argument("--num-points", type=int, default=30,
+                        help="Number of points")
     parser.add_argument("i", type=int, help="The initial prep, either 0 or 1.")
     args = parser.parse_args()
     main(args)

main.py
@@ -22,7 +22,7 @@ def prepare_filename(filename):
 
 def parse_input(nstr):
     ns = nstr.split(",")
-    ns = np.array([int(n) for n in ns]).reshape((2, 2, 2))
+    ns = np.array([int(n) for n in ns]).reshape((2, 2, 2), order='C')
     ns = np.moveaxis(ns, (0, 1, 2), (2, 1, 0))
     return ns
 
@@ -30,8 +30,7 @@ def parse_input(nstr):
 def parse_input_file(input_filename):
     with open(input_filename) as f:
         reader = csv.reader(f)
-        data = np.array([int(row[0]) for row in reader])
-    data = data.reshape((2, 2, 2))
+        data = np.array([int(row[0]) for row in reader]).reshape((2, 2, 2))
     data = np.moveaxis(data, (0, 1, 2), (2, 1, 0))
     return data
 
@@ -50,8 +49,8 @@ def main(args):
     allbounds = np.array(
         [
             [i] +
-            [f(af.data_to_point(data), args.c, i) for f in [af.upper_reference_point, af.lower_reference_point]] +
-            [f(data, alpha, args.beta, args.c, i) for alpha in alphas for f in [af.total_lower, af.total_upper]]
+            [f(af.data_to_point(data), args.c, i) for f in [af.lower_reference_point, af.upper_reference_point]] +
+            [f(data, alpha/2, args.beta, args.c, i) for alpha in alphas for f in [af.total_lower, af.total_upper]]
             for i in range(2)
         ])
     filename = prepare_filename(args.filename)